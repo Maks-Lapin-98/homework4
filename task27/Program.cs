de37bd88627939118082d4dec7e030fed9b8b9a3
maks <maks-lapin-1998@.ru>
2023-05-06 19:54:01 +0400

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,34 @@
+﻿int number = ReadInt("Введите число: ");
+System.Console.WriteLine(Sum(DoMassiv(number)));
+
+int ReadInt(string argument)
+{
+	Console.Write(argument);
+	int i;
+	while (!int.TryParse(Console.ReadLine(), out i))
+	{
+        Console.WriteLine("Это не число!");
+        Console.Write(argument);
+	}
+	return i;
+}
+int [] DoMassiv(int x)
+{
+    x = Math.Abs(x);
+    string str = x.ToString();
+    int [] S = new int [str.Length];
+    for (int i = 0; i < str.Length; i++)
+    {
+       S[i] = int.Parse(str[i].ToString()); 
+    }
+    return S;
+}
+int Sum(int[] x)
+{
+    int sum = 0;
+    for (int i = 0; i < x.Length; i++)
+    {
+        sum +=x[i];
+    }
+    return sum;
+}
