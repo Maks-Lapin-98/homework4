d2807ad730f9cccedc4941c8c3a7d74fc6c3ab5c
maks <maks-lapin-1998@.ru>
2023-05-06 19:57:17 +0400

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int [] array = new int[8];
+
+int[] FillArray()
+{
+    int Length = array.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        array[index] = new Random().Next();
+        index++;
+    }
+    return array;
+}
+
+System.Console.WriteLine(string.Join("  ", FillArray()));
